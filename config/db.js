b7b4e80ae6f672a373f1f5169f10c999585be9da
Nahim SALAMI <marcjeoge@gmail.com>
2025-04-13 13:36:32 +0100

    Refactor code structure for improved readability and maintainability

--- config/db.js
+++ config/db.js
@@ -1,12 +1,15 @@
 // config/db.js
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 // Create a MySQL connection pool
+console.log( process.env.DB_HOST, process.env.DB_USER, process.env.DB_PASS, process.env.DB_NAME, process.env.DB_PORT)
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'blyd3d@202620sA@',
-  database: 'WIFI_ZONE_WABAA',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
