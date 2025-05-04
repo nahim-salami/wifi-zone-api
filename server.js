8c55152a8201f7829fa2a264274183cd92b3910f
Nahim SALAMI <marcjeoge@gmail.com>
2025-05-04 18:10:28 +0100

    Modification du port par défaut du serveur de 3000 à 8080

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/upload', (req, res) => {
 });
 
 // 🚀 Démarrage du serveur
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`✅ Serveur démarré sur http://localhost:${PORT}`);
 });
